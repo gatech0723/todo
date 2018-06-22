e0fa768c31b544d4c30d0e7291bbf65357658250
shawnhr <shawnk0818@gmail.com>
2018-06-21 21:46:10 -0700

    Refactored before final commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,3 @@
-/*$("li").click(function(e) {
-  // if ($(this).css("color") === "rgb(128, 128, 128)") {
-  //   $(this).css({ color: "black", textDecoration: "none" });
-  // } else {
-  //   $(this).css({
-  //     color: "gray",
-  //     textDecoration: "line-through"
-  //   });
-  // }
-  $(this).toggleClass("completed");
-  e.stopPropagation();
-});*/
-
 $("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
@@ -22,31 +9,17 @@ $("ul").on("click", "span", function(e) {
     });
   e.stopPropagation();
 });
-// $("span").click(function(event) {
-//   // alert("clicked span");
-//   $(this)
-//     .parent()
-//     .fadeOut(500, function() {
-//       $(this).remove();
-//     });
-//   event.stopPropagation();
-// });
-// $("ul").click(function() {
-//   alert("clicked ul");
-// });
-// $("#container").click(function() {
-//   alert("clicked container div");
-// });
-// $("body").click(function() {
-//   alert("clicked body");
-// });
-// $("html").click(function() {
-//   alert("clicked html");
-// });
+
 $("input[type='text']").keypress(function(e) {
   if (e.which === 13) {
     var todoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X<span> " + todoText + "</li>");
+    $("ul").append(
+      "<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>"
+    );
   }
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
 });
